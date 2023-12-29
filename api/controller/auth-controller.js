17ee58757822cef18e1af1ada0b08d3e947bf813
AfaqAhmad1122 <afaqahmadnaeem@gmail.com>
2023-12-29 19:45:52 +0500

    complete sign in page functionality

--- api/controller/auth-controller.js
+++ api/controller/auth-controller.js
@@ -40,11 +40,21 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, "User not found"));
+    // Example for handling authentication failure in your signin controller
+    if (!validUser) {
+      return res
+        .status(401)
+        .json({ success: false, message: "User not found" });
+    }
+
     const validPassword = bcrypt.compareSync(password, validUser.password);
-    if (!validPassword) return next(401, "Wrong credentials");
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong credentials"));
+    }
+
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
+
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
